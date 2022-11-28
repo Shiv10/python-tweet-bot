98fc1ea153374f4516855bd6ad6eb56e69d37582
shiv10 <shivanshsharma2012@gmail.com>
2022-11-28 18:25:52 +0530

    feat: connect twitter api

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
-from dotenv import load_dotenv
-import requests
 import bs4 as bs
-import os
+from datetime import datetime
+from dotenv import load_dotenv
 import openai
+import os
+import pytz
+import requests
+import time
+import tweepy
 
 load_dotenv()
 
+def api():
+    auth = tweepy.OAuthHandler(os.getenv('TWITTER_API_KEY'), os.getenv('TWITTER_API_SECRET'))
+    auth.set_access_token(os.getenv('TWITTER_ACCESS_TOKEN'), os.getenv('TWITTER_ACCESS_TOKEN_SECRET'))
+
+    return tweepy.API(auth)
+
+def post_tweet(api: tweepy.API, message: str):
+    api.update_status(message)
+    date = datetime.now(pytz.timezone('Asia/Kolkata'))
+    print('\n\nTweeted:\n{}\nat : {}'.format(message, date))
+
+
 openai.api_key = os.getenv('OPENAI_API_KEY')
+twitterApi = api()
 
-resp = requests.get('http://www.jimprice.com/generator/generate.php')
-soup = bs.BeautifulSoup(resp.text, features='lxml')
+for i in range(50):
+    resp = requests.get('http://www.jimprice.com/generator/generate.php')
+    soup = bs.BeautifulSoup(resp.text, features='lxml')
 
-b = soup.find('b')
-term = ' '.join(b.text.strip().split())
+    b = soup.find('b')
+    term = ' '.join(b.text.strip().split())
 
-tweet = openai.Completion.create(
-  model='text-davinci-002',
-  prompt='Write a funny tweet by Elon Musk on the topic "' + str(term)+ '"',
-  max_tokens=350,
-  temperature=0.95
-)
+    tweet = openai.Completion.create(
+        model='text-davinci-002',
+        prompt='Write a funny tweet by Elon Musk on the topic "' + str(term)+ '"',
+        max_tokens=350,
+        temperature=0.95
+    )
 
-print(tweet["choices"][0]["text"].strip())
-
-
+    tweet = tweet["choices"][0]["text"].strip()
+    post_tweet(twitterApi, tweet)
+    time.sleep(60*60*24)
