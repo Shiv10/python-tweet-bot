1987359e6da82eb9718d8461e549acf9619b4fca
shiv10 <shivanshsharma2012@gmail.com>
2022-11-28 16:33:17 +0530

    feat: add openai api call to get tweet

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+from dotenv import load_dotenv
 import requests
 import bs4 as bs
+import os
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 resp = requests.get('http://www.jimprice.com/generator/generate.php')
 soup = bs.BeautifulSoup(resp.text, features='lxml')
 
 b = soup.find('b')
 term = ' '.join(b.text.strip().split())
-print(term)
+
+tweet = openai.Completion.create(
+  model='text-davinci-002',
+  prompt='Write a funny tweet by Elon Musk on the topic "' + str(term)+ '"',
+  max_tokens=350,
+  temperature=0.95
+)
+
+print(tweet["choices"][0]["text"].strip())
+
+
